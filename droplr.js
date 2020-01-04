dc34246d20db8d8331f7faedd87a8e4a7753aead
David Ellenwood <davee@tri.be>
2020-01-04 00:30:40 -0600

    Node.js only version of the app.

--- droplr.js
+++ droplr.js
@@ -1,43 +1,73 @@
 #!/usr/bin/env node
 
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 const fs = require('fs');
-const Droplr = require('droplr-api');
+const path = require('path');
 
-const config = require('./config.json');
-const argv = require('yargs').options({
-	'file': {
-		alias: 'f',
-		demandOption: true,
-		describe: 'A complete path to the screenshot file to upload.',
-		type: 'string'
-	},
-	'title': {
-		alias: 't',
-		demandOption: true,
-		describe: 'The title of the screenshot as it will appear on Droplr.',
-		type: 'string'
-	}
-}).argv;
+const Droplr = require('droplr-api');
+const moment = require('moment');
+
+const configFile = './config.json';
+
+if( !fs.existsSync(configFile) ) {
+	console.error("Couldn't find config.json. Did you remember to setup your credentials?");
+	process.exit();
+}
+
+const config = require(configFile);
+
+const state = {
+	dir: path.resolve(config.tempDir),
+	filename: `screenshot_${ moment().format('Y-MM-DD_HH:mm:ss') }.png`,
+	filePath: '',
+};
 
 const client = new Droplr.Client({
 	auth: new Droplr.BasicAuth(config.username, config.password),
 });
 
-const postClip = async () => {
-	await client.drops.create({
+const copyClipLink = (url) => {
+	exec(`echo '${url}' | xclip -selection clipboard -l 1`);
+	exec(`notify-send "Copied ${url} to clipboard"`);
+	exec(`rm ${state.filePath}`);
+};
+
+const postClip = () => {
+	client.drops.create({
 		type: 'FILE',
 		variant: 'image/png',
-		title: argv.title,
-		content: fs.createReadStream(argv.file),
+		title: state.filename,
+		content: fs.createReadStream(state.filePath),
 	}).then((response) => {
-		console.log(response.shortlink);
+		copyClipLink(response.shortlink);
 	}, (error) => {
 		console.error(error);
 	});
 }
 
-const init = () => {
+const createClip = async () => {
+	const { stdout, stderr } = await exec(`gnome-screenshot -a -f ${state.filePath}`);
+
+	if ( stderr ) {
+		console.error('stderr:', stderr);
+		return;
+	}
+
 	postClip();
+}
+
+const handleInitialState = () => {
+	if ( !fs.existsSync(state.dir) ) {
+		exec(`mkdir -p ${state.dir}`);
+	}
+
+	state.filePath = path.resolve(config.tempDir, state.filename);
+}
+
+const init = async () => {
+	await handleInitialState();
+	createClip();
 };
 
 init();
