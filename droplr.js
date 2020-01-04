0a62e4925338ed3b54397165f1aa519a9b0aff5d
David Ellenwood <davee@tri.be>
2020-01-04 13:32:29 -0600

    Improve notifications and docs.

--- droplr.js
+++ droplr.js
@@ -10,9 +10,12 @@ const moment = require('moment');
 
 const configFile = path.resolve( __dirname, 'config.json' );
 
+/**
+ * Make sure the config file exists before trying to use it.
+ */
 if( !fs.existsSync(configFile) ) {
 	//console.error("Couldn't find config.json. Did you remember to setup your credentials?");
-	exec(`notify-send "Couldn't find config.json. Did you remember to setup your credentials?"`);
+	exec(`notify-send -c transfer.error "Couldn't find config.json. Did you remember to setup your credentials?"`);
 	process.exit();
 }
 
@@ -24,16 +27,29 @@ const state = {
 	filePath: '',
 };
 
+/**
+ * Create a new authorized client for the API.
+ *
+ * @type {Client}
+ */
 const client = new Droplr.Client({
 	auth: new Droplr.BasicAuth(config.username, config.password),
 });
 
+/**
+ * Copy the screenshot's shortlink to the system clipboard and remove the temp screenshot file.
+ *
+ * @param url
+ */
 const copyClipLink = (url) => {
 	exec(`echo ${url} | tr -d '[:space:]' | xsel -ib`);
-	exec(`notify-send "Copied ${url} to clipboard"`);
+	exec(`notify-send -c transfer.complete 'Drop created' 'Share link ${url} copied to clipboard.'`);
 	exec(`rm ${state.filePath}`);
 };
 
+/**
+ * Post the screenshot to Droplr.
+ */
 const postClip = () => {
 	client.drops.create({
 		type: 'FILE',
@@ -44,22 +60,37 @@ const postClip = () => {
 		copyClipLink(response.shortlink);
 	}, (error) => {
 		//console.error(error);
-		exec(`notify-send "Unable to publish screenshot. ${error}"`);
+		exec(`notify-send -c transfer.error "Unable to publish screenshot." "${error}"`);
 	});
 }
 
+/**
+ * Create the screenshot.
+ *
+ * @returns {Promise<void>}
+ */
 const createClip = async () => {
-	const { stdout, stderr } = await exec(`gnome-screenshot -a -f ${state.filePath}`);
+	try {
+		const { stdout, stderr } = await exec( `gnome-screenshot -a -f ${state.filePath}` );
 
-	if ( stderr ) {
-		//console.error('stderr:', stderr);
-		exec(`notify-send "Unable to create screenshot. Error: ${stderr}"`);
+		if ( stderr ) {
+			//console.error('stderr:', stderr);
+			exec(`notify-send -c transfer.error "Unable to create screenshot." "${stderr}"`);
+			process.exit();
+		}
+
+	} catch ( error ) {
+		//console.error('error:', error);
+		exec(`notify-send -c transfer.error "Unable to create screenshot." "${error}"`);
 		process.exit();
 	}
 
 	postClip();
 }
 
+/**
+ * Set the state values necessary for the script to run properly.
+ */
 const handleInitialState = () => {
 	if ( !fs.existsSync(state.dir) ) {
 		exec(`mkdir -p ${state.dir}`);
@@ -68,6 +99,11 @@ const handleInitialState = () => {
 	state.filePath = path.resolve(__dirname, config.tempDir, state.filename);
 }
 
+/**
+ * Run this thing.
+ *
+ * @returns {Promise<void>}
+ */
 const init = async () => {
 	await handleInitialState();
 	createClip();
